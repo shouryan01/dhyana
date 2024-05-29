d6992088d3a603bef627a853e52a655063827495
Shouryan Nikam <dev.shouryan@gmail.com>
2024-05-28 20:53:46 -0700

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import FadeInWrapper from "@/components/fade-in-wrapper";
+import { Link } from "next-view-transitions";
+import { STAGGER_CHILD_VARIANTS } from "@/lib/utils";
+import { motion } from "framer-motion";
+
+export default function About() {
+	return (
+		<FadeInWrapper>
+			<main className="flex flex-col justify-center items-center bg-stone-200 text-stone-600 font-serif h-screen overflow-hidden">
+				<Link href="/">
+					<Button variant="ghost" className="fixed top-10 left-10">
+						back
+					</Button>
+				</Link>
+
+				<div className="flex flex-col max-w-3xl">
+					<span className="text-3xl font-serif dhyana">about</span>
+					<motion.div variants={STAGGER_CHILD_VARIANTS}>
+						<div className="text-md font-serif m-10">why build dhyāna?</div>
+						<ul className="list-none">
+							<li>many great journaling apps already exist.</li>
+							<li>pen and paper exists.</li>
+						</ul>
+
+						<div className="text-md font-serif m-10">vision:</div>
+						<ul>
+							<li>
+								help beginners put feelings into writing (i struggle with this)
+							</li>
+							<li>
+								help with self-reflection: automatically surface relevant old
+								memories
+							</li>
+							<li>be a "pseudo-therapist"</li>
+							<li>advanced AI and search features</li>
+						</ul>
+
+						<div className="text-md font-serif m-10">lastly:</div>
+						<ul className="list-none">
+							<li>dhyāna will always have a free option</li>
+							<li>dhyāna will always have a 100% local, offline option</li>
+						</ul>
+					</motion.div>
+				</div>
+			</main>
+		</FadeInWrapper>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { BadgeInfo } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import Chat from "@/components/chat";
 import DhyanaHeader from "@/components/dhyana-header";
 import FadeInWrapper from "@/components/fade-in-wrapper";
@@ -8,17 +10,6 @@ export default function Home() {
 			<main className="flex flex-col justify-between items-center bg-stone-200 text-stone-600 font-serif h-screen overflow-hidden">
 				<DhyanaHeader />
 				<Chat />
-
-				{/* <Link href="/about">
-					<Button variant="ghost" className="fixed right-0">
-						About
-						<BadgeInfo className="w-5 h-5 ml-1" />
-					</Button>
-				</Link> */}
-				{/* <div className="justify-between h-screen">
-					<DhyanaHeader />
-					<Chat />
-				</div> */}
 			</main>
 		</FadeInWrapper>
 	);
