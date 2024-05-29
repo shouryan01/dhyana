ebb1b6866c32a6b17c2aac76beef345f870f7624
Shouryan Nikam <dev.shouryan@gmail.com>
2024-05-28 20:53:40 -0700

    add next-view-transitions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import { ViewTransitions } from "next-view-transitions";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
+		<ViewTransitions>
+			<html lang="en">
+				<body className={inter.className}>{children}</body>
+			</html>
+		</ViewTransitions>
 	);
 }
